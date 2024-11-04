e9eaf97185178b1e345f6bed0b09b513b5eb686c
abdelkbir AMDDAH <abdoamdah3@gmail.com>
2024-11-04 19:43:51 +0100

    fix a problem with aws_instance server

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,70 +17,68 @@ provider "aws" {
 }
 
 resource "aws_instance" "name" {
-    ami ="ami-07db896e164bc4476"
-    instance_type ="t2.micro"
-    key_name =aws_key_pair.deployer.key_name
-    vpc_security_groups_id = [aws_security_group.maingroup.id]
-    iam_instance_profile =aws_iam_instance_profile.ec2-profile.name
-    connection{
-        type = "ssh"
-        host =self.public_api
-        user="ubuntu"
-        private_key =var.private_key
-        timeout="4m"
-    }
-
-    tag ={
-        "name" ="DeployVM"
-    }
+  ami                  = "ami-07db896e164bc4476"
+  instance_type        = "t2.micro"
+  key_name             = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  tags = {
+    "Name" = "DeployVM"
+  }
 }
+
 
 resource "aws_iam_instance_profile" "ec2-profile"{
     name ="ec2-profile"
     role = "EC2-ECR-AUTH"
     
 }
-resource "aws_security_group" "maingroup"{
-    egress =[
-        {
-            cidr_blocks = ["0.0.0.0/0"]
-            description = ""
-            from_port =0
-            ipv6_cidr_blockes =[]
-            prefix_list_ids =[]
-            protocol ="-i"
-            security_groups =[]
-            self =false
-            to_port =0
+resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks       = ["0.0.0.0/0"]
+      description       = ""
+      from_port         = 0
+      ipv6_cidr_blocks  = []
+      prefix_list_ids   = []
+      protocol          = "-1"
+      security_groups   = []
+      self              = false
+      to_port           = 0
+    }
+  ]
 
-        }
-        
-    ]
-    ingress =[
-         {
-            cidr_blocks = ["0.0.0.0/0", ]
-            description = ""
-            from_port =22
-            ipv6_cidr_blockes =[]
-            prefix_list_ids =[]
-            protocol ="tcp"
-            security_groups =[]
-            self =false
-            to_port =22
-
-        }, {
-            cidr_blocks = ["0.0.0.0/0"]
-            description = ""
-            from_port =80
-            ipv6_cidr_blockes =[]
-            prefix_list_ids =[]
-            protocol ="tcp"
-            security_groups =[]
-            self =false
-            to_port =80
-
-        }
-    ]
+  ingress = [
+    {
+      cidr_blocks       = ["0.0.0.0/0"]
+      description       = ""
+      from_port         = 22
+      ipv6_cidr_blocks  = []
+      prefix_list_ids   = []
+      protocol          = "tcp"
+      security_groups   = []
+      self              = false
+      to_port           = 22
+    },
+    {
+      cidr_blocks       = ["0.0.0.0/0"]
+      description       = ""
+      from_port         = 80
+      ipv6_cidr_blocks  = []
+      prefix_list_ids   = []
+      protocol          = "tcp"
+      security_groups   = []
+      self              = false
+      to_port           = 80
+    }
+  ]
 }
 
 resource "aws_key_pair" "deployer"{
